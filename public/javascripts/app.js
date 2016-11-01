aae2a8c7aa2984e599d3582cf895fb25acd5fe77
Preeti Viswanathan <vis.preeti@gmail.com>
2016-11-01 16:59:29 -0700

    create view and render data to page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,10 @@
 var App = {
   albumsLoaded: function() {
-
+    this.view.render();
   },
   fetchAlbums: function() {
     this.albums = new Albums();
+    this.view = new AlbumsView({ collection: this.albums });
     this.albums.fetch({ // fetch data for collection from server
       success: this.albumsLoaded.bind(this)
     });
--- public/javascripts/models/album.js
+++ public/javascripts/models/album.js
@@ -1,3 +1,6 @@
 var Album = Backbone.Model.extend({
-
+  parse: function(attrs) { // parse callback adds track_url property to model attributes
+    attrs.track_url = "/album/" + attrs.title;
+    return attrs;
+  }
 });
